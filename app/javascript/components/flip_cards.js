481ba2a62aa1931ce81ed848677adf8070fb6ab9
clement simons <clementsimons@gmail.com>
2018-04-16 17:58:00 +0200

    improving landing page

--- app/javascript/components/flip_cards.js
+++ app/javascript/components/flip_cards.js
@@ -1,36 +1,10 @@
-function flip_cards() {
 
-  const card_personal = document.querySelector(".card-personal")
-  card_personal.addEventListener("click", (event) => {
-    console.log(event);
-    console.log(event.currentTarget);
+const cards_personal = document.querySelectorAll(".personal-card")
+cards_personal.forEach (function(flip_card) {
+  flip_card.addEventListener("mouseenter", (event) => {
+    flip_card.closest(".personal-card").classList.toggle("flip");
   });
-
-}
-
-export { flip_cards };
-
-
-
-// const line_ups = document.querySelectorAll('.line-up')
-
-// const flip_btns = document.querySelectorAll(".btn-get-turn")
-// flip_btns.forEach (function(flip_btn) {
-//   flip_btn.addEventListener("click", (event) => {
-//     flip_btn.closest(".card-black").classList.toggle("flip");
-//     if ($(".line-up-close")[0]) {
-//       flip_btn.closest(".card-black").querySelector(".line-up").classList.remove("line-up-close");
-//       flip_btn.closest(".card-black").querySelector(".line-up-content").classList.remove("line-up-content-close");
-//     }
-//     flip_btn.closest(".card-black").querySelector(".line-up").classList.add("line-up-open");
-//     flip_btn.closest(".card-black").querySelector(".line-up-content").classList.add("line-up-content-open");
-//   });
-// });
-
-// const cards_black = document.querySelectorAll(".card-black .back")
-// const flip_btns = document.querySelectorAll(".btn-get-turn")
-// cards_black.forEach (function(card_black) {
-//   card_black.addEventListener("click", (event) => {
-//     event.currentTarget.closest(".card_black").classList.toggle("flip");
-//   });
-// });
+  flip_card.addEventListener("mouseleave", (event) => {
+    flip_card.closest(".personal-card").classList.toggle("flip");
+  });
+});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,3 @@
 import "bootstrap";
 import {flip_cards} from '../components/flip_cards';
+import {anchor_transition} from '../components/anchor_transition';
